6868087775fdac2a225f0c5180466de1130b3e10
gopal <gopal.agrawal-ext@simnovus.com>
2025-01-08 07:16:20 +0530

    more testcases added

--- tests/worker_pool_test.go
+++ tests/worker_pool_test.go
@@ -3,6 +3,9 @@ package test
 import (
 	"context"
 	"errors"
+	"fmt"
+	"math/rand"
+	"sync"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -80,34 +83,36 @@ func TestWorkerPoolBehavior(t *testing.T) {
 	}
 }
 
-// func TestRateLimitingAccuracy(t *testing.T) {
-// 	ctx := context.Background()
-// 	wp, _ := throttler.NewWorkerPool(ctx, throttler.WithWorkers(8), throttler.WithRateLimit(10)) // 10 tasks per second
-// 	wp.Start()
-// 	defer wp.Stop()
+func TestRateLimitingAccuracy(t *testing.T) {
+	ctx := context.Background()
+	wp, _ := throttler.NewWorkerPool[interface{}, interface{}](ctx,
+		throttler.WithWorkers[interface{}, interface{}](8),
+		throttler.WithRateLimit[interface{}, interface{}](10)) // 10 tasks per second
+	wp.Start()
+	defer wp.Stop()
 
-// 	taskCount := 50
-// 	timestamps := make([]time.Time, taskCount)
-// 	for i := 0; i < taskCount; i++ {
-// 		resultChan, _ := wp.AddTask(
-// 			throttler.TaskOptions{ID: fmt.Sprintf("task-%d", i)},
-// 			nil,
-// 			func(ctx context.Context, inputs interface{}) (interface{}, error) {
-// 				timestamps[i] = time.Now()
-// 				return nil, nil
-// 			})
-// 		<-resultChan
-// 	}
+	taskCount := 50
+	timestamps := make([]time.Time, taskCount)
+	for i := 0; i < taskCount; i++ {
+		resultChan, _ := wp.AddFunction(
+			throttler.TaskOptions{ID: fmt.Sprintf("task-%d", i)},
+			nil,
+			func(ctx context.Context, inputs interface{}) (interface{}, error) {
+				timestamps[i] = time.Now()
+				return nil, nil
+			})
+		<-resultChan
+	}
 
-//		for i := 1; i < taskCount; i++ {
-//			duration := timestamps[i].Sub(timestamps[i-1])
-//			expectedMin := time.Second / 10
-//			if duration < expectedMin*9/10 { // 10% tolerance
-//				t.Errorf("tasks ran faster than expected; interval between task %d and %d: %v", i-1, i, duration)
-//			}
-//		}
-//	}
-//
+	for i := 1; i < taskCount; i++ {
+		duration := timestamps[i].Sub(timestamps[i-1])
+		expectedMin := time.Second / 10
+		if duration < expectedMin*9/10 { // 10% tolerance
+			t.Errorf("tasks ran faster than expected; interval between task %d and %d: %v", i-1, i, duration)
+		}
+	}
+}
+
 // RetryTask implements the Executable interface and simulates retry behavior.
 type RetryTask struct {
 	RetryCounter *atomic.Int32
@@ -161,159 +166,160 @@ func TestWorkerPoolWithRetryTask(t *testing.T) {
 	}
 }
 
-// func TestHeavyLoadScenarios(t *testing.T) {
-// 	ctx := context.Background()
-// 	wp, _ := throttler.NewWorkerPool(ctx, throttler.WithWorkers[any, any](2), throttler.WithRateLimit[any, any](10000)) // 5 tasks/sec
-// 	wp.Start()
-// 	defer wp.Stop()
+func TestHeavyLoadScenarios(t *testing.T) {
+	ctx := context.Background()
+	wp, _ := throttler.NewWorkerPool(ctx,
+		throttler.WithWorkers[any, any](8),
+		throttler.WithRateLimit[any, any](100)) //100 tasks/sec
+	wp.Start()
+	defer wp.Stop()
 
-// 	numTasks := 50000
+	numTasks := 500
 
-// 	completedTasks := atomic.Int32{}
-// 	for j := 0; j < numTasks; j++ {
-// 		resultChan, _ := wp.AddTask(throttler.TaskOptions{
-// 			ID:         "retry-task",
-// 			MaxRetries: 1,
-// 			Backoff:    time.Millisecond * 10,
-// 		}, nil, func(ctx context.Context, input interface{}) (interface{}, error) {
-// 			rand.Seed(time.Now().UnixNano())
-// 			randInt := rand.Intn(300)
-// 			result := 1
+	completedTasks := atomic.Int32{}
+	for j := 0; j < numTasks; j++ {
+		resultChan, _ := wp.AddFunction(throttler.TaskOptions{
+			MaxRetries: 1,
+			Backoff:    time.Millisecond * 10,
+		}, nil, func(ctx context.Context, input interface{}) (interface{}, error) {
+			rand.Seed(time.Now().UnixNano())
+			randInt := rand.Intn(300)
+			result := 1
 
-// 			for i := 1; i <= randInt; i++ {
-// 				result *= i
-// 			}
-// 			completedTasks.Add(1)
-// 			return result, nil
-// 		})
+			for i := 1; i <= randInt; i++ {
+				result *= i
+			}
+			completedTasks.Add(1)
+			return result, nil
+		})
 
-// 		go func() {
-// 			res := <-resultChan
-// 			if res.Error != nil {
-// 				t.Errorf("task failed: %v", res.Error)
-// 			}
-// 		}()
-// 	}
+		go func() {
+			res := <-resultChan
+			if res.Error != nil {
+				t.Errorf("task failed: %v", res.Error)
+			}
+		}()
+	}
 
-// 	time.Sleep(time.Second * 5)
-// 	wp.Stop()
+	time.Sleep(time.Second * 5)
+	wp.Stop()
 
-// 	if completedTasks.Load() != int32(numTasks) {
-// 		t.Errorf("expected %d completed tasks, got %d", numTasks, completedTasks.Load())
-// 	}
-// }
+	if completedTasks.Load() != int32(numTasks) {
+		t.Errorf("expected %d completed tasks, got %d", numTasks, completedTasks.Load())
+	}
+}
 
-// func TestContextCancellation(t *testing.T) {
-// 	ctx, cancel := context.WithCancel(context.Background())
-// 	wp, _ := throttler.NewWorkerPool(context.Background(), throttler.WithWorkers[any, any](2), throttler.WithRateLimit[any, any](10)) // 10 tasks/sec
-// 	wp.Start()
-// 	defer wp.Stop()
+func TestContextCancellation(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	wp, _ := throttler.NewWorkerPool(context.Background(), throttler.WithWorkers[any, any](2), throttler.WithRateLimit[any, any](10)) // 10 tasks/sec
+	wp.Start()
+	defer wp.Stop()
 
-// 	cancelled := make(chan struct{})
-// 	resultChan, _ := wp.AddTask(throttler.TaskOptions{
-// 		ID:      "cancelled-task",
-// 		Context: ctx,
-// 	}, nil, func(ctx context.Context, inputs interface{}) (interface{}, error) {
-// 		<-ctx.Done()
-// 		close(cancelled)
-// 		return nil, ctx.Err()
-// 	})
-// 	cancel()
-// 	res := <-resultChan
+	cancelled := make(chan struct{})
+	resultChan, _ := wp.AddFunction(throttler.TaskOptions{
+		ID:      "cancelled-task",
+		Context: ctx,
+	}, nil, func(ctx context.Context, inputs interface{}) (interface{}, error) {
+		<-ctx.Done()
+		close(cancelled)
+		return nil, ctx.Err()
+	})
+	cancel()
+	res := <-resultChan
 
-// 	<-cancelled
-// 	if res.Error == nil || !errors.Is(res.Error, context.Canceled) {
-// 		t.Errorf("expected context cancellation error, got: %v", res.Error)
-// 	}
-// }
+	<-cancelled
+	if res.Error == nil || !errors.Is(res.Error, context.Canceled) {
+		t.Errorf("expected context cancellation error, got: %v", res.Error)
+	}
+}
 
-// func BenchmarkWorkerPoolExecution(b *testing.B) {
-// 	ctx := context.Background()
-// 	wp, _ := throttler.NewWorkerPool(ctx, throttler.WithWorkers[any, any](4), throttler.WithRateLimit[any, any](10000)) // High throughput
-// 	wp.Start()
-// 	defer wp.Stop()
+func BenchmarkWorkerPoolExecution(b *testing.B) {
+	ctx := context.Background()
+	wp, _ := throttler.NewWorkerPool(ctx, throttler.WithWorkers[any, any](4), throttler.WithRateLimit[any, any](10000)) // High throughput
+	wp.Start()
+	defer wp.Stop()
 
-// 	b.ResetTimer()
-// 	b.RunParallel(func(pb *testing.PB) {
-// 		for pb.Next() {
-// 			resultChan, _ := wp.AddTask(throttler.TaskOptions{
-// 				ID: "benchmark-task",
-// 			}, nil, func(ctx context.Context, inputs interface{}) (interface{}, error) {
-// 				return nil, nil
-// 			})
-// 			<-resultChan
-// 		}
-// 	})
-// }
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			resultChan, _ := wp.AddFunction(throttler.TaskOptions{
+				ID: "benchmark-task",
+			}, nil, func(ctx context.Context, inputs interface{}) (interface{}, error) {
+				return nil, nil
+			})
+			<-resultChan
+		}
+	})
+}
 
-// func TestWorkerPoolGracefulShutdown(t *testing.T) {
-// 	ctx, cancel := context.WithCancel(context.Background())
-// 	defer cancel()
+func TestWorkerPoolGracefulShutdown(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-// 	wp, err := throttler.NewWorkerPool(ctx, throttler.WithWorkers[any, any](10), throttler.WithRateLimit[any, any](20))
-// 	if err != nil {
-// 		t.Fatalf("Failed to create WorkerPool: %v", err)
-// 	}
+	wp, err := throttler.NewWorkerPool(ctx, throttler.WithWorkers[any, any](10), throttler.WithRateLimit[any, any](20))
+	if err != nil {
+		t.Fatalf("Failed to create WorkerPool: %v", err)
+	}
 
-// 	numTasks := 40
-// 	tasksCompleted := int32(0)
+	numTasks := 40
+	tasksCompleted := int32(0)
 
-// 	wp.Start()
+	wp.Start()
 
-// 	for i := 0; i < numTasks; i++ {
-// 		_, err := wp.AddTask(
-// 			throttler.TaskOptions{},
-// 			"input",
-// 			func(ctx context.Context, inputs interface{}) (interface{}, error) {
-// 				time.Sleep(10 * time.Millisecond) // Simulate work
-// 				atomic.AddInt32(&tasksCompleted, 1)
-// 				return nil, nil
-// 			},
-// 		)
-// 		if err != nil {
-// 			t.Errorf("Failed to add task: %v", err)
-// 		}
-// 	}
+	for i := 0; i < numTasks; i++ {
+		_, err := wp.AddFunction(
+			throttler.TaskOptions{},
+			"input",
+			func(ctx context.Context, inputs interface{}) (interface{}, error) {
+				time.Sleep(10 * time.Millisecond) // Simulate work
+				atomic.AddInt32(&tasksCompleted, 1)
+				return nil, nil
+			},
+		)
+		if err != nil {
+			t.Errorf("Failed to add task: %v", err)
+		}
+	}
 
-// 	// Shutdown the wp and validate tasks completed
-// 	wp.Stop()
+	// Shutdown the wp and validate tasks completed
+	wp.Stop()
 
-// 	if tasksCompleted == 0 {
-// 		t.Errorf("No tasks completed before shutdown")
-// 	}
-// 	if tasksCompleted == int32(numTasks) {
-// 		t.Errorf("Unexpected: All tasks completed even though shutdown occurred")
-// 	}
-// }
+	if tasksCompleted == 0 {
+		t.Errorf("No tasks completed before shutdown")
+	}
+	if tasksCompleted == int32(numTasks) {
+		t.Errorf("Unexpected: All tasks completed even though shutdown occurred")
+	}
+}
 
-// func TestWorkerPoolRaceCondition(t *testing.T) {
-// 	ctx, cancel := context.WithCancel(context.Background())
-// 	defer cancel()
+func TestWorkerPoolRaceCondition(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-// 	pool, err := throttler.NewWorkerPool(ctx, throttler.WithWorkers[any, any](10), throttler.WithRateLimit[any, any](100))
-// 	if err != nil {
-// 		t.Fatalf("Failed to create WorkerPool: %v", err)
-// 	}
+	pool, err := throttler.NewWorkerPool(ctx, throttler.WithWorkers[any, any](10), throttler.WithRateLimit[any, any](100))
+	if err != nil {
+		t.Fatalf("Failed to create WorkerPool: %v", err)
+	}
 
-// 	pool.Start()
+	pool.Start()
 
-// 	numTasks := 1000
-// 	var wg sync.WaitGroup
+	numTasks := 1000
+	var wg sync.WaitGroup
 
-// 	for i := 0; i < numTasks; i++ {
-// 		wg.Add(1)
-// 		go func() {
-// 			defer wg.Done()
-// 			_, _ = pool.AddTask(
-// 				throttler.TaskOptions{},
-// 				"input",
-// 				func(ctx context.Context, inputs interface{}) (interface{}, error) {
-// 					return nil, nil
-// 				},
-// 			)
-// 		}()
-// 	}
+	for i := 0; i < numTasks; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			_, _ = pool.AddFunction(
+				throttler.TaskOptions{},
+				"input",
+				func(ctx context.Context, inputs interface{}) (interface{}, error) {
+					return nil, nil
+				},
+			)
+		}()
+	}
 
-// 	wg.Wait()
-// 	pool.Stop()
-// }
+	wg.Wait()
+	pool.Stop()
+}
